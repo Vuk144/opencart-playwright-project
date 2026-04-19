ccfc9d8616bb13c9996dee4b4da8212ad6b2bb1d
Vuk <vuknedeljkovic144@gmail.com>
2026-04-19 12:43:37 +0200

    first commit from Vuk-1 branch

---
+++ tests/gittest.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { MyAccountPage } from '../pages/MyAccountPage';
+import { DataProvider } from '../utils/dataProvider';
+import { TestConfig } from '../test.config';
+import { HomePage } from '../pages/HomePage';
